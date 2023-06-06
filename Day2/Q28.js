7fb492a66b1aed908492d03e64e7782a0c6c39de
Jamesgit-8710 <84719466+Jamesgit-8710@users.noreply.github.com>
2023-06-06 18:07:41 +0530

    updated

--- Day2/Q28.js
+++ Day2/Q28.js
@@ -1,1 +1,16 @@
 let heights = [1,1,4,2,1,3]
+
+let expected = [...heights]
+expected.sort(function(a, b){return a - b});
+
+//let result = [];
+let c=0;
+
+for(let i=0; i<expected.length; i++)
+{
+    if(heights[i]!=expected[i])
+    c++
+    //result.push(i)
+}
+
+console.log(c)
