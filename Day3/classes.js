9ae442aee068298b897d3a1bab095e65cca97194
Jamesgit-8710 <84719466+Jamesgit-8710@users.noreply.github.com>
2023-06-07 18:06:31 +0530

    updated

---
+++ Day3/classes.js
@@ -0,0 +1,21 @@
+class oops{
+    
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+    }
+    #num(){
+        return this.y;
+    }
+
+    publicMethod() {
+        console.log('This is a public method');
+        console.log(this.#num()) // Private function can be accessed within the class
+    }
+
+
+}
+
+obj = new oops(2,5);
+
+console.log(obj.publicMethod())
